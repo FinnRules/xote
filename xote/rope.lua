5cc3f985d17cdff55961c35554bf1db726ff4c32
FinnRules <36964028+FinnRules@users.noreply.github.com>
2018-03-17 19:07:25 -0400

    Update rope.lua

--- xote/rope.lua
+++ xote/rope.lua
@@ -1,1 +1,43 @@
-
+--Top of rope
+minetest.register_node("xote:rope_top", {
+	description = "Rope",
+	drawtype = "signlike",
+	tiles = {"xote_rope_top.png"},
+	inventory_image = "xote_rope_top.png",
+	wield_image = "xote_rope_top.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+--bottom of rope
+minetest.register_node("xote:rope_bottom", {
+	description = "Rope",
+	drawtype = "signlike",
+	tiles = {"xote_rope_bottom.png"},
+	inventory_image = "xote_rope_bottom.png",
+	wield_image = "xote_rope_bottom.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {oddly_breakable_by_hand = 2}
+})
+--abm to create new rope beneath rope where there is air

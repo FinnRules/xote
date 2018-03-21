5017fd2636abc489e72dbc24c7f3755c033e6e34
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-21 08:48:19 -0400

    Update drinks.lua

--- xote/drinks.lua
+++ xote/drinks.lua
@@ -1,3 +1,4 @@
+--[[
 --cola (gravity)
 minetest.register_craftitem("xote:soda_cola", {
 	description = "Cola",
@@ -40,6 +41,62 @@ minetest.register_craftitem("xote:soda_orange", {
             minetest.after(30, function(player)
                 player:set_physics_override({
                     jump = 1,
+                })
+            end, player)
+})
+]]--
+--redux
+--cola
+minetest.register_node("xote:soda_cola", {
+	description = "Cola",
+	drawtype = "plantlike",
+	tiles = {"xote_soda_cola.png"},
+	inventory_image = "xote_soda_cola.png",
+	wield_image = "xote_soda_cola.png",
+	paramtype = "light",
+	is_ground_content = false,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
+	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
+	sounds = default.node_sound_glass_defaults(),
+	on_use = function(itemstack, player)
+		playerr:set_physics_override({
+                gravity= 0.1,
+            	})
+            -- undo after 30 seconds
+            minetest.after(30, function(player)
+                player:set_physics_override({
+                    gravity = 1,
+                })
+            end, player)
+})
+--orange
+minetest.register_node("xote:soda_orange", {
+	description = "Orange Soda",
+	drawtype = "plantlike",
+	tiles = {"xote_soda_orange.png"},
+	inventory_image = "xote_soda_orange.png",
+	wield_image = "xote_soda_orange.png",
+	paramtype = "light",
+	is_ground_content = false,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
+	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
+	sounds = default.node_sound_glass_defaults(),
+	on_use = function(itemstack, player)
+		playerr:set_physics_override({
+                speed= 3.0,
+            	})
+            -- undo after 30 seconds
+            minetest.after(30, function(player)
+                player:set_physics_override({
+                    speed = 1,
                 })
             end, player)
 })

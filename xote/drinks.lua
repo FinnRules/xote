157f3b7718ec26c90a8e171fcf8b248f231c2dc5
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-20 22:03:33 -0400

    Update drinks.lua

--- xote/drinks.lua
+++ xote/drinks.lua
@@ -2,20 +2,17 @@
 
 --bad code
 minetest.register_craftitem("xote:soda_cola", {
-		description = "Cola",
-		inventory_image = "xote_soda_cola.png",
-		on_use = function(itemstack, player)
-			local time = 0,
-      player:set_physics_override({
-				gravity = 0.1,
-			})
-      while time < 5000 do
-        time = time + 1,
-        if time >= 4999 then
-          player:set_physics_override({
-				    gravity = 1.0,
-			    })  
-        end
-      end
-		end
+	description = "Cola",
+	inventory_image = "xote_soda_cola.png",
+	on_use = function(itemstack, player)
+		playerr:set_physics_override({
+                gravity= 0.1,
+            	})
+            -- undo after 30 seconds
+            minetest.after(30, function(player)
+                player:set_physics_override({
+                    gravity = 1,
+                })
+            end, player)
 })
+	

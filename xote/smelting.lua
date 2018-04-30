138b6e79644b6f9bb7bd04e046bd95577b1c135a
Finn <36964028+FinnRules@users.noreply.github.com>
2018-04-30 10:41:37 -0400

    Added smelting from testextra.lua

--- xote/smelting.lua
+++ xote/smelting.lua
@@ -2,3 +2,34 @@
 Code to handle all cooking, smelting and fuel.
 ]]--
 
+--Smelting
+minetest.register_craft({
+		type = "cooking",
+		recipe = "xote:pitchblende_ore",
+		output = "xote:uranium_ingot",
+})
+
+minetest.register_craft({
+		type = "cooking",
+		recipe = "xote:galena_ore",
+		output = "xote:lead_ingot",
+})
+
+minetest.register_craft({
+		type = "cooking",
+		recipe = "xote:sphalerite_ore",
+		output = "xote:zinc_ingot",
+})
+
+minetest.register_craft({
+		type = "cooking",
+		recipe = "xote:zircon_ore",
+		output = "xote:zirconium_ingot",
+})
+
+minetest.register_craft({
+		type = "cooking",
+		recipe = "xote:zirconium_ingot",
+		output = "xote:cubic_zirconia",
+})
+
